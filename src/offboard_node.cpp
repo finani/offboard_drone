009cf2dc20188996de3922484ed555d8d8633c4a
Inhwan Wee <10ezenfinani@gmail.com>
2021-02-20 22:49:02 +0900

    .gitignore

---
+++ src/offboard_node.cpp
@@ -0,0 +1,46 @@
+#include "px4drone/px4drone.hpp"
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+  cout << fixed << setprecision(2);
+
+  ros::init(argc, argv, "offboard_node");
+  ros::NodeHandle nh;
+  Px4Drone px4drone = Px4Drone(&nh);
+  ros::Rate rate(px4drone.getRosRate());
+
+  // wait for Initialization
+  while(ros::ok() && !px4drone.doInitialization(rate)) { //TODO: add == true on everywhere like this
+    ros::spinOnce();
+    rate.sleep();
+  } 
+
+  //TODO: rosparam -> On/Off
+  // px4drone.doMission(0, -1, -1, 3, -1, -1, 1, -1, rate); // Auto arm(POSCTL) - Takeoff - OFFBOARD
+
+  cout << "main loop start" << endl;
+  while(ros::ok())
+  {
+    // system("clear");
+    //TODO: rosparam -> On/Off
+    // px4.drone.showHelp();
+    //TODO: rosparam -> On/Off
+    px4drone.showState();
+    px4drone.showOdom();
+
+    // px4drone.doMission(23, 1, 2, 3, 45, 3, 1, 1, rate);
+    px4drone.doMission(px4drone.getGoalService(), px4drone.getGoalX(), px4drone.getGoalY(), px4drone.getGoalZ(), px4drone.getGoalR(), px4drone.getGoalVXY(), px4drone.getGoalVZUp(), px4drone.getGoalVZDown(), rate);
+    //TODO: pub with rate keep calling doMission
+
+    px4drone.pubRvizTopics();
+
+    ros::spinOnce();
+    rate.sleep();
+  }
+
+  return 0;
+}
