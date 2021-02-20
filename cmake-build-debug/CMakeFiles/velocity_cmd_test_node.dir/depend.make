b052743af00dc3eecc2bf542d4be354ceff98451
Inhwan Wee <10ezenfinani@gmail.com>
2021-02-20 22:42:00 +0900

    .gitignore

--- cmake-build-debug/CMakeFiles/velocity_cmd_test_node.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for velocity_cmd_test_node.
-# This may be replaced when dependencies are built.
--- cmake-build-debug/gtest/googlemock/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/googletest/googletest
-# Build directory: /home/weebee/catkin_ws/src/offboard_drone/cmake-build-debug/gtest/googlemock/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
